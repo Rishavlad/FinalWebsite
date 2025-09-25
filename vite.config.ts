6a28c28837b9ddd5565ee43483ab8f5ab0edfc1f
Rishavlad <151812309+Rishavlad@users.noreply.github.com>
2025-09-25 23:11:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/veterans-cricket-nepal/',
+  base: '/FinalWebsite/',
   build: {
     outDir: 'dist',
   },
